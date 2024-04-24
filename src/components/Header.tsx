110daaa665d8588660bf6db8008f59ef7df86686
Higor Pires de França <higorpiresdefranca60@gmail.com>
2024-04-24 18:48:33 -0300

    chora: add Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
-import Link from "next/link";
 import { ComponentProps } from "react";
+import { AiOutlineLogout, AiOutlineUser } from "react-icons/ai";
 
 interface HeaderProps extends ComponentProps<"header"> {}
 
 export function Header(props: HeaderProps) {
   return (
-    <header className="bg-green-800 h-12 flex flex-row">
-      <nav className="h-full w-screen flex flex-row items-center justify-end px-4 gap-x-2">
-        <Link href="/">Teste</Link>
-        <Link href="/customers">Customers</Link>
+    <header className="bg-gray-800 h-12 flex flex-row">
+      <nav className="h-full w-screen flex items-center justify-end px-4 py-2 gap-x-2">
+        <div className="flex items-center justify-center h-full hover:bg-gray-900 rounded-xl px-2 gap-2">
+          <AiOutlineUser className="text-white" />
+          <span className="text-white font-bold">Usuário</span>
+        </div>
+        <button className="h-full hover:bg-red-800 bg-red-700 px-2 font-bold rounded-xl">
+          <AiOutlineLogout className="text-white" />
+        </button>
       </nav>
     </header>
   );
