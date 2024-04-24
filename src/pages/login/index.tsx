a6997732108a43f11160840c2ac48848c49f0123
Higor Pires de França <higorpiresdefranca60@gmail.com>
2024-04-24 18:49:41 -0300

    chore: login update

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,21 @@
-import { FormInput } from "./components/FormInput";
+import { FormInput } from "@/components/FormInput";
 
 export default function Login() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-around p-24">
-      <h1 className="text-4xl font-bold">Faça Login!</h1>
-      <form className="flex flex-col w-96 space-y-4">
-        <FormInput type="email" placeholder="E-mail" />
-        <FormInput type="password" placeholder="Senha" />
-        <button
-          type="submit"
-          className="p-2 bg-green-600 text-white rounded-md"
-        >
-          Entrar
-        </button>
-      </form>
+    <main className="flex h-full w-full justify-center p-8">
+      <div className="flex flex-col items-center justify-around h-full w-full sm:w-[36rem] min-w-96 bg-gray-800 rounded-lg">
+        <h1 className="text-4xl font-bold">Faça Login!</h1>
+        <form className="flex flex-col w-96 space-y-4">
+          <FormInput type="email" placeholder="E-mail" />
+          <FormInput type="password" placeholder="Senha" />
+          <button
+            type="submit"
+            className="p-2 bg-green-600 text-white rounded-md"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
