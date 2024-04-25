599951ce8f823479e334de73fd5e995e26a17491
Higor Pires de França <higorpiresdefranca60@gmail.com>
2024-04-25 00:33:08 -0300

    chore: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex flex-col min-h-full items-center justify-around p-12">
-      <h1 className="text-4xl font-bold">Inicio</h1>
+      <h1 className="text-4xl font-bold">Bem vindo</h1>
+      <p className="text-lg">
+        Este é um projeto de um desafio para demonstrar o uso de Next.js com
+        TailwindCSS.
+      </p>
+      <p>Gerenciamento de Clientes com Cartão</p>
+      <Link href="/customers">
+        <button className="p-2 bg-green-600 text-white rounded-md">
+          Acessar
+        </button>
+      </Link>
     </main>
   );
 }
