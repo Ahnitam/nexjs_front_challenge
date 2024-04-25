c9d3044bc836167375c5cdee03e3236f21d3c35d
Higor Pires de França <higorpiresdefranca60@gmail.com>
2024-04-25 00:32:05 -0300

    chore: fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps extends ComponentProps<"header"> {}
 
 export function Header(props: HeaderProps) {
   return (
-    <header className="bg-gray-800 h-12 flex flex-row">
+    <header className="bg-gray-800 h-12 min-h-12 flex flex-row">
       <nav className="h-full w-screen flex items-center justify-end px-4 py-2 gap-x-2">
         <div className="flex items-center justify-center h-full hover:bg-gray-900 rounded-xl px-2 gap-2">
           <AiOutlineUser className="text-white" />
