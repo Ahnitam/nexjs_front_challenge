b939848a738a539a0b1ab59cf4e2614d08cf5268
Higor Pires de França <higorpiresdefranca60@gmail.com>
2024-04-27 10:33:44 -0300

    chore: remove authorization

--- src/providers/FetcherProvider.tsx
+++ src/providers/FetcherProvider.tsx
@@ -14,11 +14,6 @@ const FetcherContext = createContext<FetcherContextType>({});
 export function useFetcherContext() {
   return useContext(FetcherContext);
 }
-// challenge.test
-// 2|0DxKJY0c1VKerikZoT54emnyDxK4xHbLf3R2fIcw7a698ca4
-
-// Local
-// 5|mJJ82jAg7J9PH27NnuMCZnSPOg4UJFo0ekR1X22X708766a2
 
 export function FetcherProvider({ children }: FetcherProviderProps) {
   let fetcher = useCallback(
@@ -27,8 +22,7 @@ export function FetcherProvider({ children }: FetcherProviderProps) {
         method: method || "GET",
         headers: {
           Accept: "application/json",
-          Authorization:
-            "Bearer 5|mJJ82jAg7J9PH27NnuMCZnSPOg4UJFo0ekR1X22X708766a2",
+          // Authorization: "Bearer ",
         },
       });
       if (r.status === 401) {
